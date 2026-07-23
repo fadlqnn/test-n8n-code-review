b9655df4a6ea94911cd93e9575c90713bdd02b7d
Muhammad Fadlan <131848367+fadlqnn@users.noreply.github.com>
2026-07-23 23:27:40 +0700

    AI auto-fix: perbaikan otomatis dari code review

--- app.js
+++ app.js
@@ -16,18 +16,31 @@ const products = [
     }
 ];
 
+function escapeHTML(str) {
+    return str.replace(/[&<>'"]/g, 
+        tag => ({
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            "'": '&#39;',
+            '"': '&quot;'
+        }[tag] || tag)
+    );
+}
+
 function showProducts(){
 
     let html = "";
 
-    
     for(let i = 0; i < products.length; i++){
+        const safeName = escapeHTML(products[i].name);
+        const formattedPrice = Number(products[i].price).toLocaleString('id-ID');
 
         html += `
         <div class="card">
-            <h3>${products[i].name}</h3>
+            <h3>${safeName}</h3>
             <p class="price">
-                Rp ${products[i].price}
+                Rp ${formattedPrice}
             </p>
         </div>
         `;
